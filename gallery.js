d2cd8b72ce06877b033fc6691a358bb3d8b56a90
xintara <aintara1312@gmail.com>
2025-02-18 15:46:45 -0800

    scrollwheel and style changes

--- gallery.js
+++ gallery.js
@@ -3,6 +3,7 @@ let change;
 
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
+    track.classList.add("dragging");
 }
 
 window.onmousemove = e => {
@@ -15,12 +16,24 @@ window.onmousemove = e => {
 
     const percentage = parseFloat(track.dataset.prevPercentage) + deltaConverted;
 
-    console.log(percentage);
+
+    change = Math.min(0, Math.max(-100, percentage));
+    console.log(change);
     track.style.transform = `translate(${change}%, -50%)`;
 }
 
 window.onmouseup = () => {
     track.dataset.mouseDownAt = "0";
+    track.classList.remove("dragging");
+    track.dataset.prevPercentage = change;
+}
 
+window.onwheel = e => {
+    const scrollDelta = e.deltaY / window.innerWidth * -100;
+    const percentage = parseFloat(track.dataset.prevPercentage) + scrollDelta;
+
+    console.log(percentage);
+    change = Math.min(0, Math.max(-100, percentage));
+    track.style.transform = `translate(${change}%, -50%)`;
     track.dataset.prevPercentage = change;
 }
